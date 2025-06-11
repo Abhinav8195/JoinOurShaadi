2f63fab37d7fef5d9b945095d5eeaeb4b3e6ccbc
abhinav8195 <abhianvbhatia143@gmail.com>
2025-06-11 12:04:15 +0530

    cross

--- src/pages/WeddingPage.jsx
+++ src/pages/WeddingPage.jsx
@@ -6,7 +6,7 @@ const WeddingPage = () => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div className="py-12">      
+    <div className="pt-12">      
       <Weddings title={ 'Discover Authentic Weddings'} button={false} subtitle={'Experience vibrant traditions, heartfelt ceremonies, and local culture like never before. Select a wedding you’d love to be part of.'} />
     </div>
   )
